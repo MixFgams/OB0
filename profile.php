8b7e8dfaf03f8a8547c2120cdd8e521e759a6a6d
billel hakkas <billel.hakkas@etu.univ-lyon1.fr>
2024-11-18 09:50:24 +0100

    page profil

--- profile.php
+++ profile.php
@@ -10,6 +10,49 @@
         <?php include 'pagesOutils/header.php'?>
 
         <main>
+            <div id="user">
+                <section>
+                <p>Nom</p>
+                <p>date</p>
+                <p>niveau</p>
+                <img src="pagesOutils\pdp.jpg" alt="photo de l utilisateur">
+                </section>
+            </div>
+
+            <div id="collection">
+                <section>
+                <h2>Collection</h2>
+
+                    <h3>Aventure</h3>
+                </section>
+            </div>
+
+            <div id="film">
+                <section>
+                <h2>Film</h2>
+                <img src="" alt="image film">
+                </section>
+            </div>
+
+            <div id="serie">
+                <section>
+                <h2>serie</h2>
+
+                    <img src="" alt="image serie">
+                </section>
+
+            </div>
+
+            <div id="livre">
+                <section>
+                <h2>livre</h2>
+
+                    <img src="" alt="image livre">
+                </section>
+
+            </div>
+
+
             
         </main>
     
