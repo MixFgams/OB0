99ab54c553e7ee7654111ff8ffe7ee35a3e27e27
Foudou <nathan.liegeon@etu.univ-lyon1.fr>
2024-11-27 11:04:33 +0100

    Merge de page contenu avec V1.1

--- pagesOutils/header.php
+++ pagesOutils/header.php
@@ -2,9 +2,9 @@
     <nav class="header-nav">
         <a href="index.php"><img src="img/obLogo.png" href="../index.php" alt="Logo OB"></a>
         <a href="index.php">Accueil</a>
-        <a href="communaute.php">Communauté</a>
-        <a href="catalogue.php">Catalogue</a>
-        <a href="aPropos.php">À propos</a>
+        <a href="creationArticle.php">Communauté</a>
+        <a href="aPropos.php">Catalogue</a>
+        <a href="profil.php">À propos</a>
 
         <!--A mettre dans le cas ou l'utilisateur n'est pas connecté-->
         <a href="connexion.php">connexion</a>
