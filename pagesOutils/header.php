d36151576396eaca6441594afdf6b7c416e0bd23
Marwane El Boumyaoui <marw2ne4007@gmail.com>
2024-12-28 14:07:12 +0100

    Ajout d'une barre de recherche dans le header

--- pagesOutils/header.php
+++ pagesOutils/header.php
@@ -1,13 +1,14 @@
 <header>
     <nav class="header-nav">
         <a href="index.php"><img src="img/obLogo.png" href="../index.php" alt="Logo OB"></a>
+        <input id="barreRecherche" name="barreRecherche" type="search" placeholder="Recherche un film, une serie, un anime, un livre">
         <a href="index.php">Accueil</a>
         <a href="communaute.php">Communauté</a>
         <a href="pageContenu.php">Catalogue</a>
         <a href="aPropos.php">À propos</a>
 
         <!--A mettre dans le cas ou l'utilisateur n'est pas connecté-->
-        <a href="connexion.php">connexion</a>
+        <a href="connexion.php">Connexion</a>
 
         <!--A mettre seulement si l'utilisateur est connecté-->
         <a href="profile.php">Votre Profil</a>
